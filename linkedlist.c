ad773c5dcd888ad551af7f07533293d62ac91de2
C Naveen Kumar <c.naveenkumar74@gmail.com>
2021-11-18 21:53:46 +0530

    with delete and insert option

--- linkedlist.c
+++ linkedlist.c
@@ -1,94 +1,153 @@
+// Linked list operations in C
+
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+// Create a node
 struct Node {
-    int data;
-    struct Node* next;
+  int data;
+  struct Node* next;
 };
- 
-// Program to create a simple linked
-// list with 3 nodes
-int main()
-{
-    struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
- 
-    // allocate 3 nodes in the heap
-    head = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third = (struct Node*)malloc(sizeof(struct Node));
- 
-    /* Three blocks have been allocated dynamically.
-     We have pointers to these three blocks as head,
-     second and third    
-       head           second           third
-        |                |               |
-        |                |               |
-    +---+-----+     +----+----+     +----+----+
-    | #  | #  |     | #  | #  |     |  # |  # |
-    +---+-----+     +----+----+     +----+----+
-    
-   # represents any random value.
-   Data is random because we haven’t assigned
-   anything yet  */
- 
-    head->data = 1; // assign data in first node
-    head->next = second; // Link first node with
-    // the second node
- 
-    /* data has been assigned to the data part of the first
-     block (block pointed by the head). And next
-     pointer of first block points to second. 
-     So they both are linked.
- 
-       head          second         third
-        |              |              |
-        |              |              |
-    +---+---+     +----+----+     +-----+----+
-    | 1  | o----->| #  | #  |     |  #  | #  |
-    +---+---+     +----+----+     +-----+----+   
-  */
- 
-    // assign data to second node
-    second->data = 2;
- 
-    // Link second node with the third node
-    second->next = third;
- 
-    /* data has been assigned to the data part of the second
-     block (block pointed by second). And next
-     pointer of the second block points to the third
-     block. So all three blocks are linked.
-   
-       head         second         third
-        |             |             |
-        |             |             |
-    +---+---+     +---+---+     +----+----+
-    | 1  | o----->| 2 | o-----> |  # |  # |
-    +---+---+     +---+---+     +----+----+      */
- 
-    third->data = 3; // assign data to third node
-    third->next = NULL;
- 
-    /* data has been assigned to data part of third
-    block (block pointed by third). And next pointer
-    of the third block is made NULL to indicate
-    that the linked list is terminated here.
- 
-     We have the linked list ready. 
- 
-           head   
-             |
-             |
-        +---+---+     +---+---+       +----+------+
-        | 1  | o----->|  2  | o-----> |  3 | NULL |
-        +---+---+     +---+---+       +----+------+      
-    
-     
-    Note that only head is sufficient to represent
-    the whole list.  We can traverse the complete
-    list by following next pointers.    */
- 
-    return 0;
+
+// Insert at the beginning
+void insertAtBeginning(struct Node** head_ref, int new_data) {
+  // Allocate memory to a node
+  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+  // insert the data
+  new_node->data = new_data;
+
+  new_node->next = (*head_ref);
+
+  // Move head to new node
+  (*head_ref) = new_node;
+}
+
+// Insert a node after a node
+void insertAfter(struct Node* prev_node, int new_data) {
+  if (prev_node == NULL) {
+  printf("the given previous node cannot be NULL");
+  return;
+  }
+
+  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+  new_node->data = new_data;
+  new_node->next = prev_node->next;
+  prev_node->next = new_node;
+}
+
+// Insert the the end
+void insertAtEnd(struct Node** head_ref, int new_data) {
+  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+  struct Node* last = *head_ref; /* used in step 5*/
+
+  new_node->data = new_data;
+  new_node->next = NULL;
+
+  if (*head_ref == NULL) {
+  *head_ref = new_node;
+  return;
+  }
+
+  while (last->next != NULL) last = last->next;
+
+  last->next = new_node;
+  return;
+}
+
+// Delete a node
+void deleteNode(struct Node** head_ref, int key) {
+  struct Node *temp = *head_ref, *prev;
+
+  if (temp != NULL && temp->data == key) {
+  *head_ref = temp->next;
+  free(temp);
+  return;
+  }
+  // Find the key to be deleted
+  while (temp != NULL && temp->data != key) {
+  prev = temp;
+  temp = temp->next;
+  }
+
+  // If the key is not present
+  if (temp == NULL) return;
+
+  // Remove the node
+  prev->next = temp->next;
+
+  free(temp);
+}
+
+// Search a node
+int searchNode(struct Node** head_ref, int key) {
+  struct Node* current = *head_ref;
+
+  while (current != NULL) {
+  if (current->data == key) return 1;
+  current = current->next;
+  }
+  return 0;
+}
+
+// Sort the linked list
+void sortLinkedList(struct Node** head_ref) {
+  struct Node *current = *head_ref, *index = NULL;
+  int temp;
+
+  if (head_ref == NULL) {
+  return;
+  } else {
+  while (current != NULL) {
+    // index points to the node next to current
+    index = current->next;
+
+    while (index != NULL) {
+    if (current->data > index->data) {
+      temp = current->data;
+      current->data = index->data;
+      index->data = temp;
+    }
+    index = index->next;
+    }
+    current = current->next;
+  }
+  }
+}
+
+// Print the linked list
+void printList(struct Node* node) {
+  while (node != NULL) {
+  printf(" %d ", node->data);
+  node = node->next;
+  }
+}
+
+// Driver program
+int main() {
+  struct Node* head = NULL;
+
+  insertAtEnd(&head, 1);
+  insertAtBeginning(&head, 2);
+  insertAtBeginning(&head, 3);
+  insertAtEnd(&head, 4);
+  insertAfter(head->next, 5);
+
+  printf("Linked list: ");
+  printList(head);
+
+  printf("\nAfter deleting an element: ");
+  deleteNode(&head, 3);
+  printList(head);
+
+  int item_to_find = 3;
+  if (searchNode(&head, item_to_find)) {
+  printf("\n%d is found", item_to_find);
+  } else {
+  printf("\n%d is not found", item_to_find);
+  }
+
+  sortLinkedList(&head);
+  printf("\nSorted List: ");
+  printList(head);
 }
